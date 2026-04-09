883959860e8d74ddbfbc54fb5c48050bf9f40e90
someshvar26 <88944978+someshvar26@users.noreply.github.com>
2026-04-09 17:43:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, App Engine! Testing only.'
+    return 'Hello, App Engine! Testing only CI-CD.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
